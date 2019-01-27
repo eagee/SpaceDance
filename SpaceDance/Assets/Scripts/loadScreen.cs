aec0697d44ee96ffe0331849b42fac8ee2ed8517
erackley <erackley@malwarebytes.com>
2019-01-27 14:39:54 -0500

    Added fixed.

---
+++ SpaceDance/Assets/Scripts/loadScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadScreen : MonoBehaviour
+{
+    // Detects if any key has been pressed.
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("A key or mouse click has been detected");
+            SceneManager.LoadScene("EagansScene", LoadSceneMode.Single); 
+        }
+    }
+}
